89880cce14d4588b870a46ee7afc049154ad2cf8
MaxHorstmann <MaxHorstmann@users.noreply.github.com>
2020-07-06 20:32:25 -0400

    Update Customer.cs

--- AspNetCoreApp5Minutes/web/Models/Customer.cs
+++ AspNetCoreApp5Minutes/web/Models/Customer.cs
@@ -8,5 +8,7 @@ namespace crm.Models
 		public string Email { get; set; }
 		public string Phone { get; set; }
 		public int TotalRevenue { get; set; }
+		public string City { get; set; }
+		public int OrderCount { get; set; }
 	}
 }
